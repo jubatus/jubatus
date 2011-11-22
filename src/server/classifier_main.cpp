a13d45700257ca29665c5bfcf912bbaeb94e2aa7
UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
2011-11-22 20:25:17 +0900

    a set of total APIs implemented although there're many important FIXMEs to make it work

--- src/server/classifier_main.cpp
+++ src/server/classifier_main.cpp
@@ -14,6 +14,8 @@
 #include "diffv.hpp"
 #include "server_util.hpp"
 
+#include <cmath>
+
 SET_PROGNAME("classifier");
 
 namespace jubatus {
@@ -58,7 +60,7 @@ config_data get_config(const model* m, int){
   return m->conf;
 }
 
-int save(const model*, const std::string& id){
+void save(const model*, const std::string& id){
   std::string ofile = "/tmp/";
   // FIXME: user server_argv#tmpdir
   //build_local_path_(ofile, type, id);
@@ -73,11 +75,9 @@ int save(const model*, const std::string& id){
     // ok = m->storage->save(ofs); // FIXME: this operation is constant
     ofs.close();
     LOG(INFO) << "saved to " << ofile;
-    return 0;
   }catch(const std::exception& e){
     throw e;
   }
-  return -1;
 }
 void load(model* m, const std::string& id){
   std::string ifile;
@@ -115,13 +115,56 @@ void load(model* m, const std::string& id){
 }
 
 diffv get_diff(const model*){  //FIXME
-  diffv d;
-  return d;
+  diffv ret;
+  // FIXME: how do we count the number of 'train's.
+  //  ret.count = mixer_->get_count();
+  //  m->storage->get_diff(ret.v);
+  return ret;
 }
+
+storage::val3_t mix_val3(const storage::val3_t& lhs, const storage::val3_t& rhs) {
+  return storage::val3_t(lhs.v1 + rhs.v1,
+                         lhs.v2, //FIXME!!! XXX!!! //min(lhs.v2, rhs.v2),
+                         lhs.v3 + rhs.v3);
+}
+storage::feature_val3_t mix_feature(const storage::feature_val3_t& lhs
+                                    , const storage::feature_val3_t& rhs) {
+  storage::val3_t def(0, 1, 0);
+  storage::feature_val3_t ret(lhs);
+  storage::detail::binop(ret, rhs, mix_val3, def);
+  return ret;
+}
+void mix_parameter(diffv& lhs, const diffv& rhs) {
+  storage::features3_t r(rhs.v);
+  for (size_t i = 0; i < r.size(); ++i) {
+    storage::feature_val3_t& f = r[i].second;
+    for (size_t j = 0; j < f.size(); ++j) {
+      f[j].second.v1 *= rhs.count;
+    }
+  }
+  storage::detail::binop(lhs.v, r, mix_feature);
+  lhs.count += rhs.count;
+}
+
+  // FIXME: this important code in old classifier is not implemented.
+  // we NEED to think more
+  //
+  // // average <= sum / servers_.size()
+  // float div = 1.f / static_cast<float>(sum.count);
+  // for (size_t i = 0; i < sum.v.size(); ++i) {
+  //   feature_val3_t& f = sum.v[i].second;
+  //   for (size_t j = 0; j < f.size(); ++j) {
+  //     f[j].second.v1 *= div;
+  //   }
+  // }
 int mix(const model*, const diffv& lhs, const diffv& rhs, diffv& acc){
-  return -1;//FIXME
+  mix_parameter(acc, lhs);
+  mix_parameter(acc, rhs);
+  return 0;//FIXME
 }
-int put_diff(model*, const diffv&){  //FIXME
+int put_diff(model* m, const diffv& diff){
+  reinterpret_cast<storage::local_storage_mixture*>(m->storage.get())->
+    set_average_and_clear_diff(diff.v);
   return -1;
 }
 
@@ -145,7 +188,7 @@ int main(int argc, char* argv[]) {
   serv.register_analysis<int, jubatus::classifier::config_data>(
       "get_config", &jubatus::classifier::get_config);
 
-  serv.register_analysis<std::string, int>(
+  serv.register_update<std::string>(
       "save", &jubatus::classifier::save);
   serv.register_update<std::string>(
       "load", &jubatus::classifier::load);
