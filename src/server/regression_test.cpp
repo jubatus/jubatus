8535723c6f6b0fa697d30790f2bc5218f9caab06
ODA Satoshi <oda.satoshi@gmail.com>
2012-01-17 22:07:58 +0900

    add regression_test_util.hpp

--- src/server/regression_test.cpp
+++ src/server/regression_test.cpp
@@ -28,6 +28,7 @@
 using namespace std;
 using namespace jubatus;
 
+static const string NAME = "regression_test";
 static const int PORT = 65434;
 
 namespace {
@@ -49,6 +50,78 @@ namespace {
     };
   };
 
+/*
+void my_test(const char* meth, const char* stor){ //serv2, api_classify){
+  client::regression r("localhost", PORT, 10);
+  const size_t example_size = 1000;
+  config_data c;
+  c.method = meth;
+  num_rule rule = { "*", "num" };
+  c.config.num_rules.push_back(rule);
+
+  r.set_config(NAME, c);
+
+  vector<pair<string, datum> > data;
+  make_random_data(data, example_size);
+  unsigned int res = c.train(NAME, data);
+  ASSERT_TRUE(res == data.size()); //.success);
+
+  vector<float>  values;
+  vector<datum>  datas;
+  {
+    vector<pair<float, datum> >::const_iterator it;
+    for(it = data.begin(); it != data.end(); ++it){
+      values.push_back(it->first);
+      datas.push_back(it->second);
+    }
+  }
+  vector<vector<estimate_result> > result = r.estimate(NAME, datas);
+//  ASSERT_TRUE(res.);
+  ASSERT_EQ(example_size, result.size());
+  ASSERT_EQ(data.size(), result.size());
+
+  vector<string>::const_iterator it0; //answers
+  vector<vector<estimate_result> >::const_iterator it;
+  size_t count = 0;
+  for(it = result.begin(), it0 = labels.begin();
+      it != result.end() && it0 != labels.end();
+      ++it, ++it0){
+    for (size_t i = 0; i < it->size(); ++i){
+      vector<estimate_result>::const_iterator ite;
+      // for(ite = it->begin(); ite != it->end(); ++ite){
+      //   cout << "[" << ite->label << "]" <<  endl;
+      // }
+    }
+    ASSERT_EQ(2u, it->size()); //estimate_results should have two label OK/NG
+    string most0;
+    double prob0 = DBL_MIN;
+    vector<estimate_result>::const_iterator ite;
+    for(ite = it->begin(); ite != it->end(); ++ite){
+      // get most likely label
+      if(prob0 < ite->prob || ite == it->begin()){
+        prob0 = ite->prob;
+        most0 = ite->label;
+      }
+    }
+    if(*it0 == most0){
+      count++;
+    }
+    // EXPECT_TRUE(*it0 == most0);
+    if( *it0 != most0 ){ //FIXME
+      cout << *it0  << "!=" << most0 << endl;
+      for(ite = it->begin(); ite != it->end(); ++ite){
+        cout << ite->label << "\t" << ite->prob << endl;
+      }
+    }
+  }
+  EXPECT_GE(count, result.size()-10); //num of wrong classification should be less than 1%
+
+}
+
+TEST_F(regression_test, pa){
+  my_test("PA",   "local");
+}
+*/
 
 TEST_F(regression_test, small) {
 
