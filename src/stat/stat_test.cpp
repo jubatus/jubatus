1d3c484b22fa2b7491f39324ab6337f5feafd38a
ODA Satoshi <oda.satoshi@gmail.com>
2012-01-18 23:53:48 +0900

    add stat_test.cpp

---
+++ src/stat/stat_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "stat.hpp"
+#include <pficommon/math/random.h>
+
+using namespace std;
+using namespace pfi::lang;
+
+namespace jubatus {
+
+template <typename T>
+class stat_test : public testing::Test {};
+
+TYPED_TEST_CASE_P(stat_test);
+
+TYPED_TEST_P(stat_test, trivial) {
+  TypeParam p(10);
+  p.push("test", 1.0);
+  p.push("test", 2.0);
+  p.push("test", 3.0);
+
+  EXPECT_TRUE(p.sum("test") == 6.0);
+  EXPECT_TRUE(p.max("test") == 3.0);
+  EXPECT_TRUE(p.min("test") == 1.0);
+}
+
+
+REGISTER_TYPED_TEST_CASE_P(
+    stat_test,
+    trivial);
+
+typedef testing::Types<stat::stat> stat_types;
+
+INSTANTIATE_TYPED_TEST_CASE_P(stt, stat_test, stat_types);
+
+}
