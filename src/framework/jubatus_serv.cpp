17c714600030198a7a76af55f59c70f7a55abf0c
UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
2012-05-23 13:26:14 +0900

    fix error handling in mix

--- src/framework/jubatus_serv.cpp
+++ src/framework/jubatus_serv.cpp
@@ -53,6 +53,8 @@ int jubatus_serv::start(pfi::network::mprpc::rpc_server& serv){
 
 #ifdef HAVE_ZOOKEEPER_H
   if(! a_.is_standalone()){
+    // FIXME: need to decide the log file
+    // without log output file, zkclient outputs to stderr
     zk_ = common::cshared_ptr<jubatus::common::lock_service>
       (common::create_lock_service("zk", a_.z, a_.timeout, "logfile_jubatus_serv"));
     ls = zk_;
@@ -68,6 +70,7 @@ int jubatus_serv::start(pfi::network::mprpc::rpc_server& serv){
       ht.register_node(a_.eth, a_.port);
     }
     
+    // FIXME(rethink): is this sequence correct?
     mixer_->set_zk(zk_);
     register_actor(*zk_, a_.name, a_.eth, a_.port);
     mixer_->start();
@@ -143,18 +146,25 @@ void jubatus_serv::join_to_cluster(common::cshared_ptr<jubatus::common::lock_ser
   if(not list.empty()){
     common::lock_service_mutex zlk(*z, common::ACTOR_BASE_PATH + "/" + a_.name + "/master_lock");
     while(not zlk.try_lock()){ ; }
+
+    // if you're using even any cht, you should choose from cht. otherwise random.
     size_t i = rand() % list.size();
+
     std::string ip;
     int port;
     common::revert(list[i], ip, port);
     pfi::network::mprpc::rpc_client c(ip, port, a_.timeout);
     
-    pfi::lang::function<std::string()> f = c.call<std::string()>("get_storage");
-    std::stringstream ss( f() );
+    pfi::lang::function<std::string(int)> f = c.call<std::string(int)>("get_storage");
+    std::string data = f(0);
+    LOG(INFO) << "join to cluster: " << data.size() << " bytes got from " << ip << " " << port;
+    std::stringstream ss( data );
+    
     for(size_t i = 0;i<mixables_.size(); ++i){
       mixables_[i]->clear();
       mixables_[i]->load(ss);
     }
+    DLOG(INFO) << "all data successfully loaded to " << mixables_.size() << " mixables.";
   }
 };
 
@@ -211,50 +221,26 @@ void jubatus_serv::do_mix(const std::vector<std::pair<std::string,int> >& v){
   vector<string> accs;
   //vector<string> serialized_diffs;
   clock_time start = get_clock_time();
-  
-  {
-    pfi::lang::function<std::vector<std::string>(const std::vector<std::string>, const std::vector<std::string>)>
+
+  if(v.empty()){
+    LOG(WARNING) << "no other server. ";
+    return;
+  }else{
+    pfi::lang::function<std::vector<std::string>
+                        (const std::vector<std::string>, const std::vector<std::string>)>
       f = pfi::lang::bind(&jubatus_serv::mix_agg, this, pfi::lang::_1, pfi::lang::_2);
-    
     common::mprpc::rpc_mclient c(v, a_.timeout);
-    c.call_async("get_diff", 0);
-    accs = c.join_all<std::vector<std::string> >(f);
-    c.call_async("put_diff", accs);
-    c.join_all<int>(pfi::lang::function<int(int,int)>(&framework::add<int>));
-  }
-  /*
-    for(size_t s = 0; s < v.size(); ++s ){
     try{
-          rpc_client c(v[s].first, v[s].second, a_.timeout);
-          function<vector<string>(int)> get_diff_fun = c.call<vector<string>(int)>("get_diff");
-          serialized_diffs = get_diff_fun(0);
-        }catch(std::exception& e){
-          LOG(ERROR) << e.what();
-          continue;
-        }
-        scoped_lock lk(rlock(m_)); // model_ should not be in mix (reduce)?
-        if(accs.empty()){
-          accs = serialized_diffs;
-        }else{
-          for(size_t i=0; i<mixables_.size(); ++i){
-            // FIXME: very inefficient but what sucks is type system of C++
-            mixables_[i]->reduce(serialized_diffs[i], accs[i]);
-          }
-        }
-	}*/
-      
-      /*
-      for(size_t s = 0; s < v.size(); ++s ){
-        try{
-          rpc_client c(v[s].first, v[s].second, a_.timeout);
-          function<int(vector<string>)> put_diff_fun = c.call<int(vector<string>)>("put_diff");
-          put_diff_fun(accs);
-        }catch(std::exception& e){
-          LOG(ERROR) << e.what();
-          continue;
-        }
-      }
-      */
+      c.call_async("get_diff", 0);
+      accs = c.join_all<std::vector<std::string> >(f);
+      c.call_async("put_diff", accs);
+      c.join_all<int>(pfi::lang::function<int(int,int)>(&framework::add<int>));
+    }catch(const std::runtime_error & e){
+      LOG(WARNING) << e.what() << " : mix failed";
+      return;
+    }
+  }
+
   clock_time end = get_clock_time();
   DLOG(INFO) << "mixed with " << v.size() << " servers in " << (double)(end - start) << " secs.";
   size_t s = 0;
