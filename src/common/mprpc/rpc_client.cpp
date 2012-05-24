27964bf07f5804fc667c47216a9c05ea130c24d7
Shuzo Kashihara <kashihara@preferred.jp>
2012-05-24 17:34:06 +0900

    Fixed EAGAIN handling of mprpc/async_client for non-blocking and some fixes.

--- src/common/mprpc/rpc_client.cpp
+++ src/common/mprpc/rpc_client.cpp
@@ -74,12 +74,12 @@ static void readable_callback(int fd, short int events, void* arg){
 int rpc_mclient::readable_callback(int fd, int events, async_context* ctx){
   int done = 0;
 
-  if(events == EV_READ){
+  if(events & EV_READ){
     pfi::lang::shared_ptr<async_sock> client = clients_[fd];
 
     int r = client->recv_async();
     if(r <= 0){
-      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+      if (errno != EAGAIN) {
         register_fd_readable_(ctx);
       } else {
         client->disconnected();
@@ -123,7 +123,7 @@ static void writable_callback(int fd, short int events, void* arg){
 int rpc_mclient::writable_callback(int fd, int events, async_context* ctx){
   int done = 0;
 
-  if(events == EV_WRITE){
+  if(events & EV_WRITE){
     pfi::lang::shared_ptr<async_sock> client = clients_[fd];
 
     if(client->is_connecting()){
@@ -131,7 +131,7 @@ int rpc_mclient::writable_callback(int fd, int events, async_context* ctx){
     }
 
     int r = client->send_async(ctx->buf->data(), ctx->buf->size());
-    if (r <= 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+    if (r <= 0 && errno != EAGAIN) {
       client->disconnected();
       done++;
 
