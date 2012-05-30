1099ee3e58adcae80b5f9d7377eb63a002891348
Shohei Hido <hido@preferred.jp>
2012-05-31 02:04:22 +0900

    define interface of lof_storage

--- src/storage/lof_storage.hpp
+++ src/storage/lof_storage.hpp
@@ -33,31 +33,39 @@ namespace storage{
 class lof_storage : public anomaly_storage_base {
 public:
   lof_storage();
-  lof_storage(const std::map<std::string, std::string>& config);
+  lof_storage(const std::map<std::string, std::string>& config); // config contains parameters for the underlying nearest neighbor search
   virtual ~lof_storage();
 
-  // same functions for recommender_storage
-  void set_row(const std::string& row, float norm);
-  void remove_row(const std::string& row);
-  void clear();
-  void get_all_row_ids(std::vector<std::string>& ids) const;
+  // For Analyze
+  virtual void calc_lrd(const sfv_t& query, std::pair<std::string, float>& lrd_value, size_t neighbor_num); // calculate lrd value for an unseen instance coming in Analyze
 
-  // important functions for lof
-  // for analyze
-  void calc_lrd(const sfv_t& query, std::pair<std::string, float>& lrd_value, size_t neighbor_num) const;
-  void similar_row_lrds(const sfv_t& query, std::map<std::string, float>& neighbor_lrd_values, size_t neighbor_num) const;
-  // for update
+  virtual void similar_row_kdists(const std::string& row, std::map<std::string, float>& neighbor_lrd_values, size_t neighbor_num); // call similar_row and get_kdist iteratively for collectin kdist values of neighbors
+  virtual void similar_row_lrds(const std::string& row, std::map<std::string, float>& neighbor_lrd_values, size_t neighbor_num); // call similar_row and get_lrd iteratively for collecting lrd values of neighbors
+  
+  // For Update
+  virtual void set_row(std::string& row, const sfv_t& query); // can be merged with update_row?
+  // virtual void decode_row(const std::string& row, sfv_t& corresponding_vector); // no need?
+  virtual void remove_row(const std::string& row);
+  virtual void clear();
+  virtual void get_all_row_ids(std::vector<std::string>& ids) const;
 
+  virtual void update_row(std::string& row, const sfv_t& diff);
+  virtual void similar_row(const std::string& row, std::vector<std::pair<std::string, float> >& ids, size_t neighbor_num) const; 
 
-  void similar_row(const std::vector<float>& hash,
-                    float norm,
-                   uint64_t probe_num,
-                   uint64_t ret_num,
-                   std::vector<std::pair<std::string, float> >& ids) const;
-  std::string name() const;
+  virtual std::string name() const;
 
-  //size_t table_num() const { return table_num_; }
-  //size_t all_lsh_num() const { return shift_.size(); }
+  // getter & setter & update for kdist and lrd values
+  virtual void get_kdist(std::pair<std::string, float>& kdist_value) const;
+  virtual void get_lrd(std::pair<std::string, float>& lrd_value) const;
+
+  virtual void set_kdist(const std::pair<std::string, float>& kdist_value);
+  virtual void set_lrd(const std::pair<std::string, float>& lrd_value);
+
+  virtual void update_all_kdist();
+  virtual void update_all_lrd();
+
+  virtual void update_lrd(const std::string& row);
+  virtual void update_kdist(const std::string& row);
 
   bool save(std::ostream& os);
   bool load(std::istream& is);
@@ -67,8 +75,9 @@ public:
   virtual void mix(const std::string& lhs, std::string& rhs) const;
 
 private:
-  uint32_t neighbor_num_;
-  float reverse_nn_coefficient_;
+  uint32_t neighbor_num_; // k of k-nn (no need?)
+  float reverse_nn_coefficient_; // c of ck-nn as an approx. of k-reverse-nn (no need?)
+  // storage::lof_table_t lof_table_ ; // table for storing k-dist and lrd values
 
   friend class pfi::data::serialization::access;
   template<class Ar>
