147e85e2c7592f1ccc07eef641c577f894ca6720
ODA Satoshi <oda.satoshi@gmail.com>
2012-06-12 15:46:35 +0900

    fix moment function

--- src/stat/stat_test.cpp
+++ src/stat/stat_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "stat.hpp"
 #include <pficommon/math/random.h>
+#include <math.h>
 
 using namespace std;
 using namespace pfi::lang;
@@ -21,6 +22,13 @@ TYPED_TEST_P(stat_test, trivial) {
   EXPECT_TRUE(p.sum("test") == 6.0);
   EXPECT_TRUE(p.max("test") == 3.0);
   EXPECT_TRUE(p.min("test") == 1.0);
+  EXPECT_NEAR(p.moment("test", 0 , 0.0) , 1.0, 0.1);
+  EXPECT_NEAR(p.moment("test", 1 , 0.0) , 2.0, 0.1);
+  EXPECT_NEAR(p.moment("test", 2 , 0.0) , 4.67, 0.1);
+  EXPECT_NEAR(p.moment("test", 2 , 2.0) , 0.67, 0.1);
+  EXPECT_NEAR(p.moment("test", 3 , 0.0) , 12.0, 0.1);
+  EXPECT_NEAR(p.stddev("test"), 0.82, 0.1);
+
 }
 
 
