a87edbd273efd1d1c3a4d218beb47de3c911c8bb
Seiya Tokui <beam.web@gmail.com>
2012-06-30 01:22:40 +0900

    add mix test using randomly generated data

--- src/anomaly/lof_storage_test.cpp
+++ src/anomaly/lof_storage_test.cpp
@@ -1,15 +1,23 @@
+#include <cmath>
 #include <sstream>
 #include <gtest/gtest.h>
+#include <pficommon/data/string/utility.h>
 #include <pficommon/lang/cast.h>
 #include <pficommon/lang/scoped_ptr.h>
+#include <pficommon/lang/shared_ptr.h>
+#include <pficommon/math/random.h>
+#include "../common/hash.hpp"
+#include "../common/portable_mixer.hpp"
+
 #include "../recommender/recommender_mock.hpp"
 #include "../recommender/recommender_mock_util.hpp"
 #include "lof_storage.hpp"
 
 using namespace std;
+using namespace pfi::data::string;
 using pfi::data::unordered_map;
-using pfi::lang::lexical_cast;
-using pfi::lang::scoped_ptr;
+using namespace pfi::lang;
+using namespace pfi::math::random;
 using jubatus::recommender::make_sfv;
 using jubatus::recommender::make_ids;
 
@@ -30,6 +38,19 @@ lof_storage* make_storage(uint32_t k,
   s->set_nn_engine(nn_engine);
 
   return s;
+}
+
+sfv_t make_dense_sfv(const string& s) {
+  sfv_t sfv;
+  istringstream iss(s);
+
+  size_t i = 0;
+  float x = 0;
+  while (iss >> x) {
+    sfv.push_back(make_pair(lexical_cast<string>(i++), x));
+  }
+
+  return sfv;
 }
 
 }
@@ -72,16 +93,16 @@ protected:
     rmock_ = new recommender::recommender_mock;
     storage_.reset(make_storage(2, 2, rmock_));
 
+    storage_->update_row("-1", make_sfv("1:-1"));
+    storage_->update_row("0", make_sfv("1:0"));
+    storage_->update_row("1", make_sfv("1:1"));
+    storage_->update_row("10", make_sfv("1:10"));
+
     rmock_->set_neighbor_relation(make_sfv("1:-1"), make_ids("0:1 1:2 10:11"));
     rmock_->set_neighbor_relation(make_sfv("1:0"), make_ids("-1:1 1:1 10:10"));
     rmock_->set_neighbor_relation(make_sfv("1:1"), make_ids("0:1 -1:2 10:9"));
     rmock_->set_neighbor_relation(make_sfv("1:10"), make_ids("1:9 0:10 -1:11"));
     rmock_->set_neighbor_relation(make_sfv("1:2"), make_ids("1:1 0:2 -1:3 10:8"));
-
-    storage_->update_row("-1", make_sfv("1:-1"));
-    storage_->update_row("0", make_sfv("1:0"));
-    storage_->update_row("1", make_sfv("1:1"));
-    storage_->update_row("10", make_sfv("1:10"));
 
     storage_->update_all();
   }
@@ -123,6 +144,128 @@ TEST_F(lof_storage_one_dimensional_test, collect_lrds_novel_input) {
   EXPECT_FLOAT_EQ(2/3.f, lrds["1"]);
   EXPECT_FLOAT_EQ(1/2.f, lrds["0"]);
 }
+
+
+class lof_storage_mix_test
+    : public ::testing::TestWithParam<pair<int, string> > {
+protected:
+  static map<string, string> parse_config(const string& config_str) {
+    const vector<string> lines = split(config_str, '\n');
+
+    map<string, string> config;
+    for (size_t i = 0; i < lines.size(); ++i) {
+      vector<string> cols = split(lines[i], '\t');
+      config[cols[0]] = cols[1];
+    }
+    return config;
+  }
+
+  sfv_t generate_gaussian(const string& name, const sfv_t& mean, float deviation) {
+    sfv_t sfv(mean);
+    const uint64_t seed = hash_util::calc_string_hash(name);
+    mtrand r(seed);
+
+    for (size_t i = 0; i < sfv.size(); ++i) {
+      sfv[i].second += r.next_gaussian() * deviation;
+    }
+
+    return sfv;
+  }
+
+  void update(const string& name, const sfv_t& mean, float deviation) {
+    const sfv_t x = generate_gaussian(name, mean, deviation);
+    lof_storage* storage = portable_mixer_.get_hash(name);
+    storage->update_row(name, x);
+
+    single_storage_->update_row(name, x);
+  }
+
+  void mix() {
+    portable_mixer_.mix();
+
+    string diff;
+    single_storage_->get_diff(diff);
+    single_storage_->set_mixed_and_clear_diff(diff);
+  }
+
+  virtual void SetUp() {
+    const pair<int, string>& param = GetParam();
+    const int num_models = param.first;
+    const map<string, string> config = parse_config(param.second);
+
+    storages_.resize(num_models);
+    for (int i = 0; i < num_models; ++i) {
+      storages_[i].reset(new lof_storage(config));
+    }
+    single_storage_.reset(new lof_storage(config));
+
+    for (size_t i = 0; i < storages_.size(); ++i) {
+      portable_mixer_.add(storages_[i].get());
+    }
+  }
+
+  virtual void TearDown() {
+    portable_mixer_.clear();
+    single_storage_.reset();
+  }
+
+  vector<shared_ptr<lof_storage> > storages_;
+  shared_ptr<lof_storage> single_storage_;
+  portable_mixer<lof_storage> portable_mixer_;
+};
+
+TEST_P(lof_storage_mix_test, consistency) {
+  static const size_t kNumSample = 40;
+  static const size_t kNumQuery = 1;
+  static const float kDeviation = 1;
+
+  const sfv_t mu0 = make_dense_sfv("1 1");
+  const sfv_t mu1 = make_dense_sfv("2 1");
+
+  for (size_t i = 0; i < kNumSample; ++i) {
+    update(lexical_cast<string>(i), mu0, kDeviation);
+  }
+
+  mix();  // mix the recommenders
+
+  for (size_t i = 0; i < kNumSample; ++i) {
+    update(lexical_cast<string>(i), mu0, kDeviation);
+  }
+
+  mix();  // mix the latest k-dists and lrds
+
+  for (size_t i = 0; i < kNumQuery; ++i) {
+    const sfv_t x = generate_gaussian("t" + lexical_cast<string>(i), mu1, 1);
+    float expect_lrd, actual_lrd;
+    unordered_map<string, float> expect_lrds, actual_lrds;
+
+    expect_lrd = single_storage_->collect_lrds(x, expect_lrds);
+
+    for (size_t j = 0; j < storages_.size(); ++j) {
+      actual_lrd = storages_[j]->collect_lrds(x, actual_lrds);
+      EXPECT_FLOAT_EQ(expect_lrd, actual_lrd);
+
+      for (unordered_map<string, float>::const_iterator it = expect_lrds.begin();
+           it != expect_lrds.end(); ++it) {
+        EXPECT_TRUE(actual_lrds.count(it->first));
+        EXPECT_FLOAT_EQ(it->second, actual_lrds[it->first]);
+      }
+    }
+  }
+}
+
+INSTANTIATE_TEST_CASE_P(
+    lof_storage_mix_test_instance,
+    lof_storage_mix_test,
+    ::testing::Values(
+        make_pair(3,
+                  string("anomaly:name\tlof\n"
+                         "lof:neighbor_num\t10\n"
+                         "lof:reverse_nn_num\t30\n"
+                         "name\teuclid_lsh\n"
+                         "lsh_num\t16\n"
+                         "table_num\t16\n"
+                         "bin_width\t1\n"))));
 
 }
 }
