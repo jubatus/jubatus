c13542673ff5954caafc00021b04a66c26f5ebcf
Yuya Unno <unnonouno@gmail.com>
2012-09-26 01:49:56 +0900

    append numbers to proxy function name to remove ambiguity

--- src/framework/keeper.hpp
+++ src/framework/keeper.hpp
@@ -56,22 +56,22 @@ class keeper : public pfi::network::mprpc::rpc_server {
 
   template <typename R>
   void register_random(const std::string& method_name) {
-    pfi::lang::function<R(std::string)> f = pfi::lang::bind(&keeper::template random_proxy<R>, this, method_name, pfi::lang::_1);
+    pfi::lang::function<R(std::string)> f = pfi::lang::bind(&keeper::template random_proxy0<R>, this, method_name, pfi::lang::_1);
     add(method_name, f);
   }
   template <typename R, typename A0> //, typename A1, typename A2>
   void register_random(const std::string& method_name) {
-    pfi::lang::function<R(std::string,A0)> f = pfi::lang::bind(&keeper::template random_proxy<R,A0>, this, method_name, pfi::lang::_1, pfi::lang::_2);
+    pfi::lang::function<R(std::string,A0)> f = pfi::lang::bind(&keeper::template random_proxy1<R,A0>, this, method_name, pfi::lang::_1, pfi::lang::_2);
     add(method_name, f);
   }
   template <typename R, typename A0, typename A1>//, typename A2>
   void register_random(const std::string& method_name) {
-    pfi::lang::function<R(std::string,A0,A1)> f = pfi::lang::bind(&keeper::template random_proxy<R,A0,A1>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3);
+    pfi::lang::function<R(std::string,A0,A1)> f = pfi::lang::bind(&keeper::template random_proxy2<R,A0,A1>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3);
     add(method_name,f); 
   }
   template <typename R, typename A0, typename A1, typename A2>
   void register_random(const std::string& method_name) {
-    pfi::lang::function<R(std::string,A0,A1,A2)> f = pfi::lang::bind(&keeper::template random_proxy<R,A0,A1,A2>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3, pfi::lang::_4);
+    pfi::lang::function<R(std::string,A0,A1,A2)> f = pfi::lang::bind(&keeper::template random_proxy3<R,A0,A1,A2>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3, pfi::lang::_4);
     add(method_name,f); 
   }
   
@@ -79,7 +79,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
   void register_broadcast(std::string method_name,
                           pfi::lang::function<R(R,R)> agg){//pfi::lang::function<R(R,R)>& agg) {
     pfi::lang::function<R(std::string)> f =
-      pfi::lang::bind(&keeper::template broadcast_proxy<R>, this, method_name, pfi::lang::_1, 
+      pfi::lang::bind(&keeper::template broadcast_proxy0<R>, this, method_name, pfi::lang::_1, 
                       agg);
     add(method_name, f);
   }
@@ -87,7 +87,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
   void register_broadcast(std::string method_name,
                           pfi::lang::function<R(R,R)> agg){//pfi::lang::function<R(R,R)>& agg) {
     pfi::lang::function<R(std::string, A0)> f =
-      pfi::lang::bind(&keeper::template broadcast_proxy<R, A0>, this, method_name, pfi::lang::_1, pfi::lang::_2,
+      pfi::lang::bind(&keeper::template broadcast_proxy1<R, A0>, this, method_name, pfi::lang::_1, pfi::lang::_2,
                       agg);
     add(method_name, f);
   }
@@ -96,25 +96,25 @@ class keeper : public pfi::network::mprpc::rpc_server {
   template <int N, typename R>
   void register_cht(std::string method_name, pfi::lang::function<R(R,R)> agg) {
     pfi::lang::function<R(std::string, std::string)> f =
-      pfi::lang::bind(&keeper::template cht_proxy<N,R>, this, method_name, pfi::lang::_1, pfi::lang::_2, agg);
+      pfi::lang::bind(&keeper::template cht_proxy0<N,R>, this, method_name, pfi::lang::_1, pfi::lang::_2, agg);
     add(method_name, f);
   }
   template <int N, typename R, typename A0>
   void register_cht(std::string method_name, pfi::lang::function<R(R,R)> agg) {
     pfi::lang::function<R(std::string, std::string, A0)> f =
-      pfi::lang::bind(&keeper::template cht_proxy<N,R,A0>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3, agg);
+      pfi::lang::bind(&keeper::template cht_proxy1<N,R,A0>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3, agg);
     add(method_name, f);
   }
   template <int N, typename R, typename A0, typename A1>
   void register_cht(std::string method_name, pfi::lang::function<R(R,R)> agg) {
     pfi::lang::function<R(std::string, std::string, A0, A1)> f =
-      pfi::lang::bind(&keeper::template cht_proxy<N,R, A0, A1>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3, pfi::lang::_4, agg);
+      pfi::lang::bind(&keeper::template cht_proxy2<N,R, A0, A1>, this, method_name, pfi::lang::_1, pfi::lang::_2, pfi::lang::_3, pfi::lang::_4, agg);
     add(method_name, f);
   }
   
  private:
   template <typename R>
-  R random_proxy(const std::string& method_name, const std::string& name){
+  R random_proxy0(const std::string& method_name, const std::string& name){
     //    {DLOG(INFO)<< __func__ << " " << method_name << " " << name;}
     std::vector<std::pair<std::string, int> > list;
     get_members_(name, list);
@@ -129,7 +129,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
     }
   }
   template <typename R, typename A>
-  R random_proxy(const std::string& method_name, const std::string& name, const A& arg){
+  R random_proxy1(const std::string& method_name, const std::string& name, const A& arg){
     //    {DLOG(INFO)<< __func__ << " " << method_name << " " << name;}
     std::vector<std::pair<std::string, int> > list;
     get_members_(name, list);
@@ -144,7 +144,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
     }
   }
   template <typename R, typename A0, typename A1>
-  R random_proxy(const std::string& method_name, const std::string& name, const A0& a0, const A1& a1){
+  R random_proxy2(const std::string& method_name, const std::string& name, const A0& a0, const A1& a1){
     std::vector<std::pair<std::string, int> > list;
     get_members_(name, list);
 
@@ -158,7 +158,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
     }
   }
   template <typename R, typename A0, typename A1, typename A2>
-  R random_proxy(const std::string& method_name, const std::string& name, const A0& a0, const A1& a1, const A2& a2){
+  R random_proxy3(const std::string& method_name, const std::string& name, const A0& a0, const A1& a1, const A2& a2){
     std::vector<std::pair<std::string, int> > list;
 
     get_members_(name, list);
@@ -174,7 +174,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
   }
 
   template <typename R>
-  R broadcast_proxy(const std::string& method_name, const std::string& name,
+  R broadcast_proxy0(const std::string& method_name, const std::string& name,
                     pfi::lang::function<R(R,R)>& agg) {
     //    {DLOG(INFO)<< __func__ << " " << method_name << " " << name;}
     std::vector<std::pair<std::string, int> > list;
@@ -189,7 +189,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
     }
   }
   template <typename R, typename A>
-  R broadcast_proxy(const std::string& method_name, const std::string& name, const A& arg,
+  R broadcast_proxy1(const std::string& method_name, const std::string& name, const A& arg,
                     pfi::lang::function<R(R,R)>& agg) {
     //    {DLOG(INFO)<< __func__ << " " << method_name << " " << name;}
     std::vector<std::pair<std::string, int> > list;
@@ -208,7 +208,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
 
 
   template <int N, typename R>
-  R cht_proxy(const std::string& method_name, const std::string& name, const std::string& id,
+  R cht_proxy0(const std::string& method_name, const std::string& name, const std::string& id,
               pfi::lang::function<R(R,R)>& agg) {
     std::vector<std::pair<std::string, int> > list;
     get_members_from_cht_(name, id, list, N);
@@ -222,7 +222,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
     }
   }
   template <int N, typename R, typename A0>
-  R cht_proxy(const std::string& method_name, const std::string& name, const std::string& id, const A0& arg,
+  R cht_proxy1(const std::string& method_name, const std::string& name, const std::string& id, const A0& arg,
               pfi::lang::function<R(R,R)>& agg) {
     std::vector<std::pair<std::string, int> > list;
     get_members_from_cht_(name, id, list, N);
@@ -236,7 +236,7 @@ class keeper : public pfi::network::mprpc::rpc_server {
     }
   }
   template <int N, typename R, typename A0, typename A1>
-  R cht_proxy(const std::string& method_name, const std::string& name, const std::string& id, const A0& a0, const A1& a1,
+  R cht_proxy2(const std::string& method_name, const std::string& name, const std::string& id, const A0& a0, const A1& a1,
               pfi::lang::function<R(R,R)>& agg) {
     std::vector<std::pair<std::string, int> > list;
     get_members_from_cht_(name, id, list, N);
