52cbf68985a675281914d1a5c8146755b93b2ae1
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2012-11-07 14:18:50 +0900

    fix reference to ATOMIC_I8_SUPPORT and add test (fix #163)

--- src/common/global_id_generator.cpp
+++ src/common/global_id_generator.cpp
@@ -21,18 +21,85 @@
 
 #ifndef ATOMIC_I8_SUPPORT
 #include <pficommon/concurrent/lock.h>
+#include <pficommon/concurrent/mutex.h>
 #endif
 
 namespace jubatus { namespace common {
 
+class global_id_generator_impl {
+ private:
+  bool is_standalone_;
+  uint64_t counter_;
+  std::string path_;
+  cshared_ptr<lock_service> ls_;
+
+#ifndef ATOMIC_I8_SUPPORT
+  pfi::concurrent::mutex counter_mutex_;
+#endif
+
+ public:
+  global_id_generator_impl():
+    is_standalone_(true),
+    counter_(0)
+  {}
+
+  global_id_generator_impl(bool is_standalone):
+    is_standalone_(is_standalone),
+    counter_(0)
+  {}
+
+  uint64_t generate()
+  {
+    if(is_standalone_){
+
+#ifdef ATOMIC_I8_SUPPORT
+      return __sync_fetch_and_add(&counter_, 1);
+#else
+      pfi::concurrent::scoped_lock lk(counter_mutex_);
+      return ++counter_;
+#endif
+
+    }else{
+
+#ifdef HAVE_ZOOKEEPER_H
+      uint64_t res;
+      if (ls_->create_id(path_, 0, res))
+        return res;
+      else
+        throw JUBATUS_EXCEPTION(jubatus::exception::runtime_error("Failed to create id"));
+#else
+      // never reaches here
+      assert(is_standalone_);
+      return 0; // dummy to remove warning
+#endif
+
+    }
+  }
+
+  void set_ls(cshared_ptr<lock_service>& ls,
+              const std::string& path_prefix)
+  {
+
+#ifdef HAVE_ZOOKEEPER_H
+    if (!is_standalone_) {
+      path_ = path_prefix + "/id_generator";
+      ls_ = ls;
+      if (!ls_->create(path_))
+        throw JUBATUS_EXCEPTION(jubatus::exception::runtime_error("Failed to create global id generator")
+            << jubatus::exception::error_api_func("lock_service::create")
+            << jubatus::exception::error_message(path_));
+    }
+#endif
+
+  }
+};
 
 global_id_generator::global_id_generator():
-  is_standalone_(true),
-  counter_(0)
+  pimpl_(new global_id_generator_impl())
 {}
+
 global_id_generator::global_id_generator(bool is_standalone):
-  is_standalone_(is_standalone),
-  counter_(0)
+  pimpl_(new global_id_generator_impl(is_standalone))
 {}
 
 global_id_generator::~global_id_generator()
@@ -40,44 +107,13 @@ global_id_generator::~global_id_generator()
 
 uint64_t global_id_generator::generate()
 {
-  if(is_standalone_){
-#ifdef ATOMIC_I8_SUPPORT
-    return __sync_fetch_and_add(&counter_, 1);
-#else
-    pfi::concurrent::scoped_lock lk(counter_mutex_);
-    return ++counter_;
-#endif
-
-  }else{
-#ifdef HAVE_ZOOKEEPER_H
-
-    uint64_t res;
-    if (ls_->create_id(path_, 0, res))
-      return res;
-    else
-      throw JUBATUS_EXCEPTION(jubatus::exception::runtime_error("Failed to create id"));
-
-#else
-    // never reaches here
-    assert(is_standalone_);
-    return 0; // dummy to remove warning
-#endif
-  }
+  return pimpl_->generate();
 }
 
 void global_id_generator::set_ls(cshared_ptr<lock_service>& ls,
                                  const std::string& path_prefix)
 {
-#ifdef HAVE_ZOOKEEPER_H
-  if (!is_standalone_) {
-    path_ = path_prefix + "/id_generator";
-    ls_ = ls;
-    if (!ls_->create(path_))
-      throw JUBATUS_EXCEPTION(jubatus::exception::runtime_error("Failed to create global id generator")
-          << jubatus::exception::error_api_func("lock_service::create")
-          << jubatus::exception::error_message(path_));
-  }
-#endif
+  pimpl_->set_ls(ls, path_prefix);
 }
 
 }}
