1de6f61c72957bcbb0412504f33a1c2ff5719ac8
Shuzo Kashihara <kashihara@preferred.jp>
2012-11-26 19:04:02 +0900

    Fix keeper to finalize zookeeper before exiting: #184

--- src/framework/keeper.cpp
+++ src/framework/keeper.cpp
@@ -16,6 +16,7 @@
 
 #include "keeper.hpp"
 
+#include <cstdlib>
 #include <iostream>
 #include <glog/logging.h>
 
@@ -60,6 +61,7 @@ keeper::~keeper(){
 int keeper::run()
 {
   try {
+    ::atexit(jubatus::framework::atexit);
     jubatus::util::set_exit_on_term();
     jubatus::util::ignore_sigpipe();
     if (this->serv(a_.port, a_.threadnum)) {
