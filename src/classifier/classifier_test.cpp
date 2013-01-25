dfa5865cd43557bf962440e3b18475004fe8d023
IMAMASU Ryohei <imms.ryohei@gmail.com>
2013-01-25 10:27:45 +0900

    fix include order of gtest.h in classifier

--- src/classifier/classifier_test.cpp
+++ src/classifier/classifier_test.cpp
@@ -14,8 +14,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#include <gtest/gtest.h>
-
 #include <algorithm>
 #include <fstream>
 #include <sstream>
@@ -23,6 +21,7 @@
 #include <utility>
 #include <vector>
 
+#include <gtest/gtest.h>
 #include <pficommon/lang/cast.h>
 
 #include "classifier_factory.hpp"
