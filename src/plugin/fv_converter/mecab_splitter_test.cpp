7e3d594bda42291f9e2d770593a22e1c0de42c56
Seiya Tokui <beam.web@gmail.com>
2013-01-29 14:28:46 +0900

    fix include order in tests of splitter plugin

--- src/plugin/fv_converter/mecab_splitter_test.cpp
+++ src/plugin/fv_converter/mecab_splitter_test.cpp
@@ -14,11 +14,11 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#include <gtest/gtest.h>
 #include <map>
 #include <string>
 #include <utility>
 #include <vector>
+#include <gtest/gtest.h>
 #include <pficommon/concurrent/thread.h>
 #include <pficommon/lang/bind.h>
 #include <pficommon/lang/scoped_ptr.h>
