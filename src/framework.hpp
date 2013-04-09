eb9fe757066291d77106d71abda66fe73f101297
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2013-04-09 11:54:26 +0900

    eliminate "#pragma once" (fix #313)

--- src/framework.hpp
+++ src/framework.hpp
@@ -14,9 +14,12 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#pragma once
+#ifndef JUBATUS_FRAMEWORK_HPP_
+#define JUBATUS_FRAMEWORK_HPP_
 
 // #include "config.hpp"
 #include "framework/mixable.hpp"
 #include "framework/server_helper.hpp"
 #include "framework/server_util.hpp"
+
+#endif  // JUBATUS_FRAMEWORK_HPP_
