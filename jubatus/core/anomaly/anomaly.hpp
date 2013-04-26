64677aee7682cd586b192c7c10b0c48ca2fbf637
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2013-04-26 13:24:51 +0900

    fix include guard in anomaly

--- jubatus/core/anomaly/anomaly.hpp
+++ jubatus/core/anomaly/anomaly.hpp
@@ -14,9 +14,9 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef JUBATUS_ANOMALY_ANOMALY_HPP_
-#define JUBATUS_ANOMALY_ANOMALY_HPP_
+#ifndef JUBATUS_CORE_ANOMALY_ANOMALY_HPP_
+#define JUBATUS_CORE_ANOMALY_ANOMALY_HPP_
 
 #include "lof.hpp"
 
-#endif  // JUBATUS_ANOMALY_ANOMALY_HPP_
+#endif  // JUBATUS_CORE_ANOMALY_ANOMALY_HPP_
--- jubatus/core/anomaly/lof_storage.hpp
+++ jubatus/core/anomaly/lof_storage.hpp
@@ -14,8 +14,8 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef JUBATUS_ANOMALY_LOF_STORAGE_HPP_
-#define JUBATUS_ANOMALY_LOF_STORAGE_HPP_
+#ifndef JUBATUS_CORE_ANOMALY_LOF_STORAGE_HPP_
+#define JUBATUS_CORE_ANOMALY_LOF_STORAGE_HPP_
 
 #include <iosfwd>
 #include <string>
@@ -182,4 +182,4 @@ class lof_storage : public anomaly_storage_base {
 }  // namespace core
 }  // namespace jubatus
 
-#endif  // JUBATUS_ANOMALY_LOF_STORAGE_HPP_
+#endif  // JUBATUS_CORE_ANOMALY_LOF_STORAGE_HPP_
