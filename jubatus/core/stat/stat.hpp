7a66ae38f6f8aca97548fa30f037a2f17a65dc92
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2013-04-26 13:29:18 +0900

    fix include guard in stat

--- jubatus/core/stat/stat.hpp
+++ jubatus/core/stat/stat.hpp
@@ -14,8 +14,8 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef JUBATUS_CORE_SRC_STAT_STAT_HPP_
-#define JUBATUS_CORE_SRC_STAT_STAT_HPP_
+#ifndef JUBATUS_CORE_STAT_STAT_HPP_
+#define JUBATUS_CORE_STAT_STAT_HPP_
 
 #include <stdint.h>
 #include <algorithm>
@@ -170,4 +170,4 @@ class stat {
 }  // namespace core
 }  // namespace jubatus
 
-#endif  // JUBATUS_CORE_SRC_STAT_STAT_HPP_
+#endif  // JUBATUS_CORE_STAT_STAT_HPP_
