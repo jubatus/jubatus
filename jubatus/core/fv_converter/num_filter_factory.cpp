6ac0103d2237c21a9c83a9fa7231116c0bedd824
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2013-07-30 18:45:14 +0900

    Modify num_filter_factory::create to return shared_ptr

--- jubatus/core/fv_converter/num_filter_factory.cpp
+++ jubatus/core/fv_converter/num_filter_factory.cpp
@@ -23,29 +23,32 @@
 #include "num_filter_impl.hpp"
 #include "util.hpp"
 
+using pfi::lang::shared_ptr;
+
 namespace jubatus {
 namespace core {
 namespace fv_converter {
 
 namespace {
 
-add_filter* create_add_filter(
+shared_ptr<add_filter> create_add_filter(
     const std::map<std::string, std::string>& params) {
   const std::string& value = get_or_die(params, "value");
   double float_val = pfi::lang::lexical_cast<double>(value);
-  return new add_filter(float_val);
+  return shared_ptr<add_filter>(new add_filter(float_val));
 }
 
-num_filter* create_dynamic_filter(
+shared_ptr<num_filter> create_dynamic_filter(
     const std::map<std::string, std::string>& params) {
   const std::string& path = get_or_die(params, "path");
   const std::string& function = get_or_die(params, "function");
-  return new dynamic_num_filter(path, function, params);
+  return shared_ptr<num_filter>(
+      new dynamic_num_filter(path, function, params));
 }
 
 }  // namespace
 
-num_filter* num_filter_factory::create(
+shared_ptr<num_filter> num_filter_factory::create(
     const std::string& name,
     const std::map<std::string, std::string>& params) const {
   if (name == "add") {
