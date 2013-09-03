d68fa04fb87e73a82e0c12baa57f2ae75b9b1628
Yuya Unno <unno@preferred.jp>
2013-09-03 12:48:27 +0900

    Insert include

--- jubatus/core/fv_converter/dynamic_loader_test.cpp
+++ jubatus/core/fv_converter/dynamic_loader_test.cpp
@@ -14,6 +14,8 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+#include <map>
+#include <string>
 #include <gtest/gtest.h>
 #include "dynamic_loader.hpp"
 #include "exception.hpp"
