18abc0427ece526c211889cede9137c6e01e8baa
Shuzo Kashihara <kashihara@preferred.jp>
2013-10-23 15:37:32 +0900

    Add regexp_splitter for builtin word splitter
    Make re2_splitter builtin function and create new splitter
    which supported oniguruma library.

--- jubatus/core/fv_converter/re2_splitter.hpp
+++ jubatus/core/fv_converter/re2_splitter.hpp
@@ -14,8 +14,8 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef JUBATUS_PLUGIN_FV_CONVERTER_RE2_SPLITTER_HPP_
-#define JUBATUS_PLUGIN_FV_CONVERTER_RE2_SPLITTER_HPP_
+#ifndef JUBATUS_CORE_FV_CONVERTER_RE2_SPLITTER_HPP_
+#define JUBATUS_CORE_FV_CONVERTER_RE2_SPLITTER_HPP_
 
 #include <map>
 #include <string>
@@ -23,15 +23,15 @@
 #include <vector>
 #include <pficommon/lang/scoped_ptr.h>
 #include <re2/re2.h>
-#include "jubatus/core/fv_converter/word_splitter.hpp"
+#include "word_splitter.hpp"
 
 namespace jubatus {
-namespace plugin {
+namespace core {
 namespace fv_converter {
 
-class re2_splitter : jubatus::core::fv_converter::word_splitter {
+class regexp_splitter : public word_splitter {
  public:
-  re2_splitter(const std::string& regexp, int group);
+  regexp_splitter(const std::string& regexp, int group);
   void split(
       const std::string& str,
       std::vector<std::pair<size_t, size_t> >& bounds) const;
@@ -42,12 +42,7 @@ class re2_splitter : jubatus::core::fv_converter::word_splitter {
 };
 
 }  // namespace fv_converter
-}  // namespace plugin
+}  // namespace core
 }  // namespace jubatus
 
-extern "C" {
-jubatus::plugin::fv_converter::re2_splitter* create(
-    const std::map<std::string, std::string>& args);
-}
-
-#endif  // JUBATUS_PLUGIN_FV_CONVERTER_RE2_SPLITTER_HPP_
+#endif  // JUBATUS_CORE_FV_CONVERTER_RE2_SPLITTER_HPP_
