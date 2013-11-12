dfe3f111a1a4eb384d712ea541f6a088fe2f8502
Yuya Unno <unno@preferred.jp>
2013-11-12 15:24:48 +0900

    Fix test script to report error code automatically

--- tools/jenerator/test/test.sh
+++ tools/jenerator/test/test.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 mkdir -p testwork
 pushd testwork
-../../src/jenerator -l server -t ../sample.idl -n toolong::name::space || exit 1
+../../src/jenerator -l server -t ../sample.idl -n toolong::name::space
 mv sample_serv.tmpl.cpp sample_serv.cpp
 mv sample_serv.tmpl.hpp sample_serv.hpp
 g++ -Wall -o sample_server sample_serv.cpp sample_impl.cpp `pkg-config --cflags --libs jubatus`
