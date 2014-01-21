be2dce2247e9ec3949bd4ebfb8ec523c6708da01
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2014-01-21 18:45:41 +0900

    recover lost chagnes in 41f4d9a; refs #611

--- jubatus/server/framework/server_util.cpp
+++ jubatus/server/framework/server_util.cpp
@@ -145,7 +145,8 @@ server_argv::server_argv(int args, char** argv, const std::string& type)
                      make_ignored_help("learning machine instance name"),
                      false);
   p.add<std::string>("mixer", 'x',
-                     make_ignored_help("mixer strategy"), false, "");
+                     make_ignored_help("mixer strategy"), false,
+                     "linear_mixer");
   p.add<int>("interval_sec", 's',
              make_ignored_help("mix interval by seconds"), false, 16,
              lower_bound_reader(0));
