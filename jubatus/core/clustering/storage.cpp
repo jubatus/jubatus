23c54569b2165641f02f93f35fd1432b3ff209ac
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2014-03-25 18:16:03 +0900

    Add dummy clear for clustering

--- jubatus/core/clustering/storage.cpp
+++ jubatus/core/clustering/storage.cpp
@@ -86,6 +86,11 @@ void storage::mix(const diff_t& lhs, diff_t& ret) {
   std::copy(lb, le, std::back_inserter(ret));
 };
 
+void storage::clear() {
+  // TODO(gintenlabo): consider revisions
+  common_.clear();
+}
+
 size_t storage::get_revision() {
   return revision_;
 }
