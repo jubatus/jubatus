d049c117fa4d360c567edb6ed288203005ff8e1d
Shuzo Kashihara <kashihara@preferred.jp>
2014-05-09 13:22:40 +0900

    Fix codestyle

--- jubatus/server/fv_converter/dynamic_loader.cpp
+++ jubatus/server/fv_converter/dynamic_loader.cpp
@@ -14,12 +14,13 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+#include "dynamic_loader.hpp"
+
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <iostream>
 #include <string>
 #include "jubatus/core/fv_converter/exception.hpp"
-#include "dynamic_loader.hpp"
 
 using jubatus::core::fv_converter::converter_exception;
 
