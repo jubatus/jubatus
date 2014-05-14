7c380c91557b160bd68211ac5dca20d1f3c22753
Shuzo Kashihara <kashihara@preferred.jp>
2014-05-14 14:00:17 +0900

    Fix license term in push_mixable

--- jubatus/core/framework/push_mixable.hpp
+++ jubatus/core/framework/push_mixable.hpp
@@ -1,3 +1,4 @@
+// Jubatus: Online machine learning framework for distributed environment
 // Copyright (C) 2014 Preferred Infrastructure and Nippon Telegraph and Telephone Corporation.
 //
 // This library is free software; you can redistribute it and/or
@@ -11,8 +12,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-//USA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #ifndef JUBATUS_CORE_FRAMEWORK_PUSH_MIXABLE_HPP_
 #define JUBATUS_CORE_FRAMEWORK_PUSH_MIXABLE_HPP_
