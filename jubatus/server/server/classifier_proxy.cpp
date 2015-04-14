5043a5341c254d89392efaafb30a488f8468204c
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2015-04-14 16:56:35 +0900

    remove giant lock on classifier (fix #968)

--- jubatus/server/server/classifier_proxy.cpp
+++ jubatus/server/server/classifier_proxy.cpp
@@ -1,4 +1,4 @@
-// This file is auto-generated from classifier.idl(0.6.4-145-g5c1292f) with jenerator version 0.6.4-146-g79178f8/feature/save_returns_id_and_path
+// This file is auto-generated from classifier.idl(0.7.2-27-ge903f57) with jenerator version 0.6.4-146-g79178f8/develop
 // *** DO NOT EDIT ***
 
 #include <map>
