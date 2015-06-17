34864c7d3162ba9d4be47b2c2bb3d630b0e9c384
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2015-06-17 15:59:33 +0900

    Regenerate

--- jubatus/server/server/anomaly_impl.cpp
+++ jubatus/server/server/anomaly_impl.cpp
@@ -1,4 +1,4 @@
-// This file is auto-generated from anomaly.idl(0.7.1-13-g481c7df) with jenerator version 0.7.1-14-g79178f8/develop
+// This file is auto-generated from anomaly.idl(0.7.2-50-gbcc1e21) with jenerator version 0.7.1-14-g79178f8/develop
 // *** DO NOT EDIT ***
 
 #include <map>
