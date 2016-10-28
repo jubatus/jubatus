3925a73c956adc68431ca98536f32dac7f84c717
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-10-28 20:18:21 +0900

    fix missing DO NOT EDIT sentence

--- jubatus/server/server/clustering_impl.cpp
+++ jubatus/server/server/clustering_impl.cpp
@@ -1,4 +1,5 @@
 // This file is auto-generated from clustering.idl(0.9.4-19-gc665909) with jenerator version 0.8.5-6-g5a2c923/feature/refactoring_clustering_api
+// *** DO NOT EDIT ***
 
 #include <map>
 #include <string>
