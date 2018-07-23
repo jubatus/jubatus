f8d58b9bcdd8451a9683e7039a6bfd25fe67f07e
IMAMASU Ryohei <imms.ryohei@gmail.com>
2018-07-23 11:45:25 +0900

    Use double in image feature plugin

--- plugin/src/fv_converter/image_feature/image_feature.hpp
+++ plugin/src/fv_converter/image_feature/image_feature.hpp
@@ -45,7 +45,7 @@ class image_feature:public jubatus::core::fv_converter::binary_feature {
     void add_feature(
       const std::string& key,
       const std::string& value,
-      std::vector<std::pair<std::string, float> >& ret_fv) const;
+      std::vector<std::pair<std::string, double> >& ret_fv) const;
     void dense_sampler(
       const cv::Mat mat,
       const int step,
